a15d43cd6e28d7bc1cc10d454f87f84c0661a770
sheraz064-ai <sherazsiddiqui0456@gmail.com>
2022-12-18 16:15:59 +0500

    Use vercel/ncc

--- validators/lintPR.js
+++ validators/lintPR.js
@@ -3,8 +3,7 @@ const github = require("@actions/github");
 
 module.exports = async function lintPR(client, pr) {
   const prTitleRegex = core.getInput("title-regex");
-  console.log(prTitleRegex)
-  const regExp = new RegExp(/^(FEATURE|FIX|TASK)\s\|\sISSUE\s\#[1-9]{1,}\s\|\s[\w\s]*$/, "gm");
+  const regExp = new RegExp(prTitleRegex, "gm");
   const isTitleValid = regExp.test(pr.title);
   if (!isTitleValid) {
     await client.rest.issues.createComment({
